a2948ad802213b775a3ef43921dcb7c413cca5d3
JOAO VITOR RODRIGUES DOS SANTOS <joaords202@gmail.com>
2026-05-06 23:56:57 -0300

    Improve number presence check in the list
    Refactor number checking logic to use a boolean flag.

--- Array_lista_9/at_4.java
+++ Array_lista_9/at_4.java
@@ -5,21 +5,26 @@ import java.util.Scanner;
 public class at_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("digite a quantidades de numeros que vc quer digitar");
+          System.out.println("digite a quantidades de numeros que vc quer digitar");
         int num = sc.nextInt();
         int[] vert = new int[num];
         int i;
+        boolean check = false;
+        System.out.println("Digite os números");
         for (i = 0; i < num; i++) {
             vert[i] = sc.nextInt();
         }
         System.out.println("Agora digite um outro número e diremos se ele está ou não na lista");
         int num1 = sc.nextInt();
+
         for (i = 0; i < num; i++){
+
             if (vert[i]==num1) {
                 System.out.println("Está presente na lista");
-                num1=0;
-            } else {num1=1;}
-        } if (num1!=0){
+                check = true;
+            }
+        }
+        if (!check){
             System.out.println("Não está na lista");
         }
         sc.close();
